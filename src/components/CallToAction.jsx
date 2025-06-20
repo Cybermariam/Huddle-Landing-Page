4ce0bc33aea894c8a0827f948c5609e12160978f
Cybermariam <ademolamariam95@gmail.com>
2025-06-20 17:44:04 +0100

    button edited

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -6,7 +6,7 @@ const CallToAction = () => {
       <h3 className="text-3xl sm:text-3xl font-bold text-[#00252e] max-w-sm mx-auto md:max-w-xl lg:text-[45px] lg:max-w-3xl mb-5">
         Ready To Build Your Community?
       </h3>
-      <button className="bg-[hsl(322,100%,66%)] text-white rounded-[2rem] px-6 py-3 text-md font-semibold mt-5 w-[45%] md:w-[50%] lg:w-[25%] lg:text-xl hover:bg-pink-300 transition duration-300">
+      <button className="bg-[hsl(322,100%,66%)] text-white rounded-[2rem] px-6 py-3 text-md font-semibold mt-5 w-[40%] md:w-[50%] lg:w-[25%] lg:text-xl hover:bg-pink-300 transition duration-300">
         Get Started For Free
       </button>
     </section>
